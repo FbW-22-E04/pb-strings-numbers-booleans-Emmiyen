7ac4d5d7c7b6dbfb80baf247848e5104f0c570fb
Danica Yener <ny21037@googlemail.com>
2022-06-29 15:03:10 +0200

    seconde

--- solution.js
+++ solution.js
@@ -1,1 +1,36 @@
+// 1
+const str = 'Danica';
+console.log(str);
+// 2
+console.log(str[0], str[5])
 
+
+//3
+
+const firstName = 'Antonio';
+
+//4
+const age = 29;
+
+ //5
+ const year = 1992;
+
+
+ //6
+
+console.log(firstName,'is', age, 'and was born in', year);
+
+//7
+let isMarried = false;
+//8
+console.log(firstName, 'is married:', isMarried);
+
+//9
+const programmingLanguage = 'JavaScript'
+const isFun = true;
+console.log(programmingLanguage, 'is fun:', isFun);
+
+//10
+console.log(programmingLanguage[0], programmingLanguage[4]);
+
+console.log(programmingLanguage.length);
